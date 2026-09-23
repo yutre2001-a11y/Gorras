dc0f243a77ee5a19682f6c215aa5078225bf735c
losprocegor <yutre2001@gmail.com>
2026-09-23 19:55:39 +0200

    Add App.tsx to src

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Navbar } from './components/Navbar'
+import { Hero } from './components/Hero'
+import { TrustStrip } from './components/TrustStrip'
+import { Inventory } from './components/Inventory'
+import { PaymentCalculator } from './components/PaymentCalculator'
+import { TradeIn } from './components/TradeIn'
+import { HowBuyingWorks } from './components/HowBuyingWorks'
+import { ServiceDepartment } from './components/ServiceDepartment'
+import { Reviews } from './components/Reviews'
+import { FinalCTA } from './components/FinalCTA'
+import { Footer } from './components/Footer'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#12141a]">
+      <Navbar />
+      <main>
+        <Hero />
+        <TrustStrip />
+        <Inventory />
+        <PaymentCalculator />
+        <TradeIn />
+        <HowBuyingWorks />
+        <ServiceDepartment />
+        <Reviews />
+        <FinalCTA />
+      </main>
+      <Footer />
+    </div>
+  )
+}
